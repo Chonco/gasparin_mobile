c802455a47e4a411a9363aa19abba8df38ea1bb0
Bruno Hernandez <bruno@bridgecode.dev>
2022-06-08 19:40:45 -0500

    seller view bottom navigation

--- app/src/main/java/com/brunocarlos/inputmanagement/SellerView.kt
+++ app/src/main/java/com/brunocarlos/inputmanagement/SellerView.kt
@@ -2,10 +2,19 @@ package com.brunocarlos.inputmanagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class SellerView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seller_view)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.seller_views_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.seller_bottom_nav)
+        setupWithNavController(bottomNavigationView, navController)
     }
 }
