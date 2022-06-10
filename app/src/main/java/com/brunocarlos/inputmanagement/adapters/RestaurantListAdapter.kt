629a53475efbfebe48dd97820e2bb02451328052
Bruno Hernandez <bruno@bridgecode.dev>
2022-06-09 19:44:00 -0500

    adapter for recycler view

---
+++ app/src/main/java/com/brunocarlos/inputmanagement/adapters/RestaurantListAdapter.kt
@@ -0,0 +1,100 @@
+package com.brunocarlos.inputmanagement.adapters
+
+import android.app.Activity
+import android.view.*
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.brunocarlos.inputmanagement.R
+import com.brunocarlos.inputmanagement.models.User
+
+class RestaurantListAdapter(
+    restaurants: List<User>,
+    layout: Int,
+    activity: Activity
+) : RecyclerView.Adapter<RestaurantListAdapter.ViewHolder>() {
+
+    private val restaurants: List<User>
+    private val layout: Int
+    private val activity: Activity
+
+    init {
+        this.restaurants = restaurants
+        this.layout = layout
+        this.activity = activity
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view),
+        View.OnCreateContextMenuListener, MenuItem.OnMenuItemClickListener {
+        private val logo: ImageView
+        private val name: TextView
+        private val foodTypesContainer: LinearLayout
+        private val address: TextView
+
+        init {
+            logo = view.findViewById(R.id.restaurant_logo)
+            name = view.findViewById(R.id.restaurant_name)
+            foodTypesContainer = view.findViewById(R.id.restaurant_food_types_container)
+            address = view.findViewById(R.id.restaurant_address)
+        }
+
+        fun bind(restaurant: User) {
+            logo.setImageBitmap(restaurant.getLogoAsBitmap())
+            name.text = restaurant.name
+            address.text = restaurant.address
+
+            val layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+            layoutParams.marginEnd = 5
+
+            for (i in 0..restaurant.foodType.size) {
+                val textView = TextView(activity)
+                textView.text = restaurant.foodType[i]
+                textView.layoutParams = layoutParams
+                foodTypesContainer.addView(textView)
+            }
+        }
+
+        override fun onCreateContextMenu(
+            contextMenu: ContextMenu,
+            view: View,
+            menuInfo: ContextMenu.ContextMenuInfo
+        ) {
+            val restaurantSelected = restaurants[this.adapterPosition]
+
+            contextMenu.setHeaderTitle(restaurantSelected.name)
+            val inflater = activity.menuInflater
+            inflater.inflate(R.menu.restaurant_list_context_menu, contextMenu)
+
+            for (i in 0..contextMenu.size()) {
+                contextMenu.getItem(i).setOnMenuItemClickListener(this)
+            }
+        }
+
+        override fun onMenuItemClick(menuItem: MenuItem): Boolean {
+            return when (menuItem.itemId) {
+                R.id.restaurant_details -> {
+                    true
+                }
+                R.id.restaurant_make_offer -> {
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(activity).inflate(layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(restaurants[position])
+    }
+
+    override fun getItemCount(): Int = restaurants.size
+}
--- app/src/main/java/com/brunocarlos/inputmanagement/models/User.kt
+++ app/src/main/java/com/brunocarlos/inputmanagement/models/User.kt
@@ -1,12 +1,21 @@
 package com.brunocarlos.inputmanagement.models
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Base64
+
 class User(
     val id: Int,
     val name: String,
     val address: String,
     val logoImg: String,
-    email: String,
-    phone: String,
-    userType: UserType,
-    foodType: List<String>
-)
+    val email: String,
+    val phone: String,
+    val userType: UserType,
+    val foodType: List<String>
+) {
+    fun getLogoAsBitmap(): Bitmap {
+        val imageBytes = Base64.decode(logoImg, Base64.DEFAULT)
+        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+    }
+}
