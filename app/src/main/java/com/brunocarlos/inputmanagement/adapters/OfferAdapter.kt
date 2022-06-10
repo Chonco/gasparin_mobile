1b0191caecccf8ed9ef4e8b447550535e5e8fe8e
Carlos Diaz <kusagary_jinfire16@outlook.com>
2022-06-09 19:49:52 -0500

    Punto de restauracion

--- app/src/main/java/com/brunocarlos/inputmanagement/adapters/OfferAdapter.kt
+++ app/src/main/java/com/brunocarlos/inputmanagement/adapters/OfferAdapter.kt
@@ -1,16 +1,21 @@
 package com.brunocarlos.inputmanagement.adapters
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.brunocarlos.inputmanagement.R
 import com.brunocarlos.inputmanagement.models.Offer
 
-class OfferAdapter(val offerList:List<Offer>): RecyclerView.Adapter<OfferViewHolder>(){
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OfferViewHolder {
+class OfferAdapter(private val offerList:List<Offer>): RecyclerView.Adapter<OfferViewHolder>(){
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OfferViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return OfferViewHolder(layoutInflater.inflate(R.layout.item_offer, parent, false))
     }
 
     override fun onBindViewHolder(holder: OfferViewHolder, position: Int) {
-
+        val item = offerList[position]
+        holder.render(item)
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/brunocarlos/inputmanagement/adapters/OfferViewHolder.kt
+++ app/src/main/java/com/brunocarlos/inputmanagement/adapters/OfferViewHolder.kt
@@ -1,4 +1,23 @@
 package com.brunocarlos.inputmanagement.adapters
 
-class OfferViewHolder {
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.brunocarlos.inputmanagement.R
+import com.brunocarlos.inputmanagement.models.Offer
+
+class OfferViewHolder (view : View): RecyclerView.ViewHolder(view){
+
+    val offerName = view.findViewById<TextView>(R.id.offerName)
+    val producerName = view.findViewById<TextView>(R.id.producerName)
+    val offerPrice = view.findViewById<TextView>(R.id.offerPrice)
+    val offerImg = view.findViewById<ImageView>(R.id.imgOffer)
+
+    fun render(offerModel : Offer){
+        offerName.text = offerModel.name
+        producerName.text = offerModel.sellerName
+        offerPrice.text = offerModel.price.toString()
+
+    }
 }
